9facfa8bf5a0d14662b1a4a9d3c81f3d90f91026
Jon Gillespie <jongillespie@outlook.com>
2020-08-07 22:45:20 +0300

    Data cleaned, hypertable created, data inserted.

--- grpc_timeseries_server/clean_csv_data.py
+++ grpc_timeseries_server/clean_csv_data.py
@@ -1,26 +1,30 @@
-''' gRPC Server for TimescaleDB '''
+""" Clean CSV Data via Pandas Dataframe """
 # TODO > Fill out the author details in the right format.
 
 import pandas as pd
 
 
-'''
-Clean the CSV File using a Pandas Dataframe
-'''
-# Directly import the local CSV file as type String
-df = pd.read_csv(f"meterusage.csv", dtype=str)
-# Confirm dataframe created, its shape and the meterusage column's type
-print(df)
-print(df.shape)
-print(df.meterusage.describe())
-# Change the meterusage datatype to float and convert errors to NaN
-df['meterusage'] = df['meterusage'].apply(pd.to_numeric, errors='coerce', downcast='float')
-# Drop any rows containing NaN and reset the frame's index
-df = df.dropna()
-df = df.reset_index(drop=True)
-# Confirm its shape and the meterusage column's type has changed
-print(df)
-print(df.shape)
-print(df.meterusage.describe())
+def clean_csv():
+    """[summary]
 
-# TODO - convert the time colum into 'time' format.
+    Returns:
+        [Pandas Dataframe]: [Contains clean records, no NaN]
+    """
+    # Directly import the local CSV file as type String
+    df = pd.read_csv(f"grpc_timeseries_server/meterusage.csv", dtype=str)
+    # Confirm dataframe created, its shape and the meterusage column's type
+    # print(df)
+    # print(df.shape)
+    # print(df.meterusage.describe())
+    # Change the meterusage datatype to float and convert errors to NaN
+    df['meterusage'] = df['meterusage'].apply(pd.to_numeric, errors='coerce', downcast='float')
+    # Convert time data to timestamp type
+    df['time'] = pd.to_datetime(df['time'])
+    # Drop any rows containing NaN and reset the frame's index
+    df = df.dropna()
+    df = df.reset_index(drop=True)
+    # Confirm its shape and the meterusage column's type has changed
+    # print(df)
+    # print(df.shape)
+    # print(df.meterusage.describe())
+    return df
--- grpc_timeseries_server/timescale_server.py
+++ grpc_timeseries_server/timescale_server.py
@@ -1,13 +1,18 @@
 """
-# TODO describe file.
+Initialise the Timescale Hypertable & populate with cleaned 'meterusage' data.
 """
 
-import psycopg2
 import os
 
-print(os.environ)
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pds
 
-meter = 'meterusage' #os.getenv('DBNAME')
+import clean_csv_data
+
+
+db_table_name = 'meter_usage'
+
 
 # Connect to the database
 CONNECTION = f"postgres://postgres:None@localhost:5432/meterusage"
@@ -15,9 +20,12 @@ CONNECTION = f"postgres://postgres:None@localhost:5432/meterusage"
 # CONNECTION = f"dbname={os.getenv('DBNAME')} user={os.getenv('DBUSER')} host=localhost port=5432 connect_timeout=10" # sslmode=require password=secret"
 # CONNECTION = f"postgres://{os.getenv('DBUSER')}:{os.getenv('DBPASS')}@{os.getenv('DBHOST')}:{os.getenv('DBPORT')}/{os.getenv('DBNAME')}"
 db_connection = psycopg2.connect(CONNECTION)
+# To utilise 'to_sql' for the dataframe, create an SQLAlchemy Engine w/ Connection
+alchemy_engine = create_engine(CONNECTION)
+postgreSQLConnection = alchemy_engine.connect()
 
 
-def db_cursor_execute(queries, multiple_queries=False):
+def db_cursor_execute(queries, is_multiple_queries=False):
     """Timescale Cursor Method for executing one or more queries.
 
     Args:
@@ -28,7 +36,7 @@ def db_cursor_execute(queries, multiple_queries=False):
     try:
         cursor = db_connection.cursor()
         print("> DB Cursor activated")
-        if multiple_queries:
+        if is_multiple_queries:
             for query in queries:
                 print("> Running Query: ", query)
                 cursor.execute(query)
@@ -46,16 +54,35 @@ def create_meterusage_hypertable():
     """Initialises the 'meterusage' hypertable within the TimescaleDB 
     """    
     
-    query_create_meterusage_table = """CREATE TABLE meter_usage (
+    query_create_meterusage_table = f"""CREATE TABLE {db_table_name} (
                                             time        TIMESTAMPTZ     NOT NULL,
                                             meterusage  FLOAT           NOT NULL
                                             );"""
-    query_create_meterusage_hypertable = "SELECT create_hypertable ('meter_usage', 'time');"
+    query_create_meterusage_hypertable = f"SELECT create_hypertable ('{db_table_name}', 'time');"
     queries = [query_create_meterusage_table, query_create_meterusage_hypertable]
-    db_cursor_execute(queries, multiple_queries=True)
-    
+    db_cursor_execute(queries, is_multiple_queries=True)
+
+
+def insert_cleaned_meterusage_data():
+    """Inserts all of the meterusage data provided by the original CSV, cleaned via pandas dataframe
+    """    
+
+    print('> Cleaning CSV Data via Dataframe')
+    cleaned_data = clean_csv_data.clean_csv()
+    # cleaned_data.to_sql(name=f'{db_table_name}', con=db_connection, schema='postgres', if_exists='replace', index=False)
+    try:
+        print('> Translating Dataframe into Database')
+        cleaned_data.to_sql(db_table_name, postgreSQLConnection, if_exists='replace')
+    except ValueError as vx:
+        print(vx)
+    except Exception as ex:  
+        print(ex)
+
+
 
 create_meterusage_hypertable()
+insert_cleaned_meterusage_data()
 
 
-
+# get_all = f"SELECT * FROM {db_table_name}"
+# db_cursor_execute([get_all])
